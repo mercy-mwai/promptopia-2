b837c4d73597a5c1eee95aa467d73f8d5c9389b2
mercy-mwai <mercyneemam@gmail.com>
2025-03-11 11:19:14 +0300

    created components to use in our project

--- app/page.jsx
+++ app/page.jsx
@@ -7,6 +7,7 @@ const Home = () => {
       </h1>
       <p className="desc text-center">Promptopia is an AI-open source AI prompting tool for modern world to discover,
          create and share creative prompts</p>
+         {/*feed */}
     </section>
   )
 }
