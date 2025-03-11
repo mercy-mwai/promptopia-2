d891ec5036bd0689bc3a10c866b4a2b495f842d5
mercy-mwai <mercyneemam@gmail.com>
2025-03-11 10:46:14 +0300

    commit message

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+/*import '../styles/globals.css'; // Use relative path
 
-const layout = () => {
+
+export const metaData ={
+    title: "Promptopia",
+    decription:"Discover & share AI prompts"
+}
+const RootLayout = ({children}) => {
   return (
-    <div>layout</div>
+  <html lang='en'>
+    <div className='main'>
+        <div className='gradient' />
+    </div>
+    
+    <main className='app'>
+        {children}
+    </main>
+  </html>
   )
 }
 
-export default layout
+export default RootLayout;*/
+import '../styles/globals.css'; // Use relative path
+
+export const metaData = {
+  title: 'Promptopia',
+  decription: 'Discover & share AI prompts',
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">{children}</main>
+      </body>
+    </html> // Remove the extra space or newline here
+  );
+};
+
+export default RootLayout;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      './pages/**/*.{js,ts,jsx,tsx,mdx}',
-      './components/**/*.{js,ts,jsx,tsx,mdx}',
-      './app/**/*.{js,ts,jsx,tsx,mdx}',
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-          satoshi: ['Satoshi', 'sans-serif'],
-          inter: ['Inter', 'sans-serif'],
-        },
-        colors: {
-          'primary-orange': '#FF5722',
-        }
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        'primary-orange': '#FF5722',
+      },
+      spacing: {
+        '16': '4rem',
       },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
