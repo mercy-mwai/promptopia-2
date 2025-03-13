fb31926c9f3c49fb288daa61eb4f6781997cc121
mercy-mwai <mercyneemam@gmail.com>
2025-03-13 11:24:14 +0300

    Add NextAuth.js authentication with Google OAuth

--- app/auth/[...nextauth]/route.js
+++ app/auth/[...nextauth]/route.js
@@ -1,18 +1,27 @@
  import NextAuth from "next-auth";
  import GoogleProvider from "next-auth/providers/google";
 
+ console.log({
+    clientId: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_SECRET_CLIENT
+})
+
  const handler =NextAuth({
     providers:[
         GoogleProvider({
-            clientId:" ",
-            clientSecret: " "
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET_CLIENT,
         })
     ],
     async session({session}){
 
     },
     async signin({profile}){
+        try{
 
+        }catch(error){
+
+        }
     }
  })
 
