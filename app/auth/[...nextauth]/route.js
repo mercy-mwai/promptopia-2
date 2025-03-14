92812b2df65985fde24f4b704e64050a4788840c
mercy-mwai <mercyneemam@gmail.com>
2025-03-14 11:03:52 +0300

    modified route.js

--- app/auth/[...nextauth]/route.js
+++ app/auth/[...nextauth]/route.js
@@ -4,10 +4,7 @@
 
  import { connectToDB } from "@/utils/database";
 
- console.log({
-    clientId: process.env.GOOGLE_ID,
-    clientSecret: process.env.GOOGLE_SECRET_CLIENT
-})
+ 
 
  const handler =NextAuth({
     providers:[
@@ -17,18 +14,34 @@
         })
     ],
     async session({session}){
-
+        const sessionUser=await User.findOne({
+            email: session.user.email
+        })
+        session.user.id=sessionUser._id.toString();
+        return session;
     },
+
     async signin({profile}){
         try{
             await connectToDB()
 
             //check if user already exists
+            const UserExists= await User.findOne({
+                email:profile.email
+            });
 
             //if not , create a new  user
+            if(!UserExists){
+                await User.create({
+                    email:email.profile,
+                    username:profile.name.replace(" ", "").toLowerCase(),
+                    image:profile.picture
+                })
+            }
 
             return true;
         }catch(error){
+            console.log("Error during sign-in:", error);
             return false;
         }
     }
