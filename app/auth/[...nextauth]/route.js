d6731ac0ae6077dea49583e4e7d08be05efbc422
mercy-mwai <mercyneemam@gmail.com>
2025-03-14 09:59:57 +0300

    Add User model with validation and unique email constraint

--- app/auth/[...nextauth]/route.js
+++ app/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
  import NextAuth from "next-auth";
  import GoogleProvider from "next-auth/providers/google";
+ import User from "@/models/user";
 
  import { connectToDB } from "@/utils/database";
 
