d52186928d701af8ac52c8ba62f81431bd5134da
mercy-mwai <mercyneemam@gmail.com>
2025-03-21 12:23:10 +0300

    commit message

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import Link from "next/link"
 
-const Form = () => {
+const Form = ({type, post,submitting,handleSubmit}) => {
   return (
-    <div>Form</div>
+    <section className="w-full max-w-full flex-start flex-col"> 
+      <h1 className="head_text text-left"> <span className="blue_gradient">{type} Post</span> </h1>
+      <p className="desc text-left">
+        {type} and share amazing prompts with the world, and let your imagination run wild with an AI-powered prompt
+      </p>
+    </section>
   )
 }
 
