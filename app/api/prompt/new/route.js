5dfe88153004f98ca6b049a18c7b000ee9fe59b0
mercy-mwai <mercyneemam@gmail.com>
2025-03-23 11:41:09 +0300

    updates

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,12 +1,18 @@
 import { connectToDB } from "@/utils/database";
 import Prompt from "@/models/prompt";
 
-export const POST= async(req)=>{
-    const {UserID, prompt, response}= await req.json();
-    try{
-        await connectToDB();
-    }catch(error){
-        
-    }
-    
-}
+export const POST = async (req) => {
+  const { UserID, prompt, response } = await req.json();
+  try {
+    await connectToDB();
+    const newPrompt = new Prompt({
+      creator: UserID,
+      prompt,
+      tag,
+    });
+    await newPrompt.save();
+    return new Response(JSON.stringify(newPrompt),{status:201})
+  } catch (error) {
+    return new Response("Failed to create prompt" , {status:500})
+  }
+};
